d65022228e2b68beb56b25fe2d23c9dbc0d23f4c
CN0105 <cn0105@CN0105.lan>
2019-06-06 10:30:48 +0100

    My first commit. Sounds with text.

---
+++ app.js
@@ -0,0 +1,47 @@
+// Play drum by keyboard press
+
+// Play music by mouse click
+const boom = () => {
+    let sound = document.getElementById("boom")
+    sound.play()
+}
+
+const clap = () => {
+    let sound = document.getElementById("clap")
+    sound.play()
+}
+
+const hihat = () => {
+    let sound = document.getElementById("hihat")
+    sound.play()
+}
+
+const kick = () => {
+    let sound = document.getElementById("kick")
+    sound.play()
+}
+
+const openhat = () => {
+    let sound = document.getElementById("openhat")
+    sound.play()
+}
+
+const ride = () => {
+    let sound = document.getElementById("ride")
+    sound.play()
+}
+
+const snare = () => {
+    let sound = document.getElementById("snare")
+    sound.play()
+}
+
+const tink = () => {
+    let sound = document.getElementById("tink")
+    sound.play()
+}
+
+const tom = () => {
+    let sound = document.getElementById("tom")
+    sound.play()
+}
