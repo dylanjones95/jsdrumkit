30d661423f4108717d0c9d01e52ef57011b28923
CN0105 <cn0105@CN0105.lan>
2019-06-07 10:06:58 +0100

    Finished HTML and JS

--- app.js
+++ app.js
@@ -1,47 +1,72 @@
+const boom = new Audio('sounds/boom.wav')
+const clap = new Audio('sounds/clap.wav')
+const hihat = new Audio('sounds/hihat.wav')
+const kick = new Audio('sounds/kick.wav')
+const openhat = new Audio('sounds/openhat.wav')
+const ride = new Audio('sounds/ride.wav')
+const snare = new Audio('sounds/snare.wav')
+const tink = new Audio('sounds/tink.wav')
+const tom = new Audio('sounds/tom.wav')
+
 // Play drum by keyboard press
+document.addEventListener("keypress", ()=> {
+    if (event.code == 'KeyG') {
+       boom.load(), boom.play()
+    } else if (event.code == 'KeyA') {
+       clap.load(), clap.play()
+    } else if (event.code == 'KeyS') {
+       hihat.load(), hihat.play()
+    } else if (event.code == 'KeyD') {
+       kick.load(), kick.play()
+    } else if (event.code == 'KeyF') {
+       openhat.load(), openhat.play()
+    } else if (event.code == 'KeyH') {
+       ride.load(), ride.play()
+    } else if (event.code == 'KeyJ') {
+       snare.load(), snare.play()
+    } else if (event.code == 'KeyL') {
+       tink.load(), tink.play()
+    } else if (event.code == 'KeyK') {
+       tom.load(), tom.play()
+    } else {
+        return false
+    }
+})  
 
-// Play music by mouse click
-const boom = () => {
-    let sound = document.getElementById("boom")
-    sound.play()
-}
 
-const clap = () => {
-    let sound = document.getElementById("clap")
-    sound.play()
-}
+// Play drum by click
+document.getElementById("boom").addEventListener("click", ()=> {
+    boom.load(), boom.play()
+    })
+        
+document.getElementById("clap").addEventListener("click", ()=> {
+    clap.load(), clap.play()
+    })
 
-const hihat = () => {
-    let sound = document.getElementById("hihat")
-    sound.play()
-}
+document.getElementById("hihat").addEventListener("click", ()=> {
+    hihat.load(), hihat.play()
+    })
 
-const kick = () => {
-    let sound = document.getElementById("kick")
-    sound.play()
-}
+document.getElementById("kick").addEventListener("click", ()=> {
+    kick.load(), kick.play()
+    })       
+                        
+document.getElementById("openhat").addEventListener("click", ()=> {
+    openhat.load(), openhat.play()
+    })    
 
-const openhat = () => {
-    let sound = document.getElementById("openhat")
-    sound.play()
-}
+document.getElementById("ride").addEventListener("click", ()=> {
+    ride.load(), ride.play()
+    })    
 
-const ride = () => {
-    let sound = document.getElementById("ride")
-    sound.play()
-}
+document.getElementById("snare").addEventListener("click", ()=> {
+    snare.load(), snare.play()
+    }) 
 
-const snare = () => {
-    let sound = document.getElementById("snare")
-    sound.play()
-}
+document.getElementById("tink").addEventListener("click", ()=> {
+    tink.load(), tink.play()
+    }) 
 
-const tink = () => {
-    let sound = document.getElementById("tink")
-    sound.play()
-}
-
-const tom = () => {
-    let sound = document.getElementById("tom")
-    sound.play()
-}
+document.getElementById("tom").addEventListener("click", ()=> {
+    tom.load(), tom.play()
+    }) 
